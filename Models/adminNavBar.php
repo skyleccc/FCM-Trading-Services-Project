1eeaf8c29fd411497c22b94dc37fbc49e73ad0b6
Justin Alec M. Antonio <justinantonio787@yahoo.com>
2024-07-05 23:17:23 +0800

    Separate file for admin dashboard

---
+++ Models/adminNavBar.php
@@ -0,0 +1,18 @@
+<?php
+
+// File Redirects
+$homeView = "/Pages/Admin/main.php";
+$calendarView = "";
+$projectsView = "/Pages/Admin/projects.php";
+$quotreqsView = "/Pages/Admin/quotationreqs.php";
+
+echo '
+<div class="col-sm-2 p-4 border bg-light"><img src="../../WebsitePictures/fcmlogo.png" alt="fcm logo" style="width: 180px;"><br><br>
+    <a href="'.$homeView.'" style="text-decoration: none; color: black;"><div><span class="material-symbols-outlined">home</span> Home</div><br></a>
+    <a href="'.$calendarView.'" style="text-decoration: none; color: black;"><div><span class="material-symbols-outlined">calendar_month</span> Calendar</div><br></a>
+    <a href="'.$projectsView.'" style="text-decoration: none; color: black;"><div><span class="material-symbols-outlined">inbox</span> Projects</div><br></a>
+    <a href="'.$quotreqsView.'" style="text-decoration: none; color: black;"><div><span class="material-symbols-outlined">request_quote</span>Quotation Requests</div><br></a>
+</div>
+'
+
+?>
