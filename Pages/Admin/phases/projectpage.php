cdea30d41205802efd9feb720d9459d62796153f
Justin Alec M. Antonio <justinantonio787@yahoo.com>
2024-07-06 19:10:57 +0800

    Remove crazy in projectpage.php

--- Pages/Admin/phases/projectpage.php
+++ Pages/Admin/phases/projectpage.php
@@ -3,10 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require '../../../Controllers/accessDatabase.php';
-<<<<<<<< HEAD:Pages/Admin/ProjectDetails/projectpage.php
 require '../../../Controllers/loginCheck.php';
-========
->>>>>>>> ac77bee5262ae29cab7060defd866ab409c8c99a:Pages/Admin/phases/projectpage.php
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -104,11 +101,7 @@ $result2 = $conn->query($sql); // edit nga ang query kay para sa mga quotation r
         <div class="row">
             <?php
             // Navigation Bar for Admin Dashboard
-<<<<<<<< HEAD:Pages/Admin/ProjectDetails/projectpage.php
             include '../../../Components/adminNavBar.php'
-========
-            include '../../../Models/adminNavBar.php'
->>>>>>>> ac77bee5262ae29cab7060defd866ab409c8c99a:Pages/Admin/phases/projectpage.php
             ?>
             <div class="col-sm-10 p-3 border bg light">
                 <div class="row p-3 border bg light">
